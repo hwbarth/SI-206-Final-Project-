492a15edb33cc9f3840a10f7b9033d90938e2656
hwbarth <92326670+hwbarth@users.noreply.github.com>
2022-04-12 22:35:29 -0400

    Add files via upload

---
+++ FINAL_PROJECT.py
@@ -0,0 +1,54 @@
+'''
+FINAL PROJECT:
+
+Golfing Brothers
+
+Harrison Barth
+Kagan Shetterly
+
+SI 206
+
+April 26, 2022
+
+Golf Leaderboard API:
+https://rapidapi.com/sportcontentapi/api/golf-leaderboard-data
+
+PGA Tour player statistics:
+https://www.pgatour.com/stats.html
+
+
+'''
+
+def getGolfLeaderBoard():
+    pass
+
+def getAvgDrivingDistance():
+    
+    pass
+
+def getAvgGreensInReg():
+    pass
+
+def getStrokesGainedOffTee():
+    pass
+
+def getScrablingPct():
+    pass
+
+def getStrokesGainedPutting():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("python")
